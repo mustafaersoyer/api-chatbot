6a9c7e983f1443399f42c951393d503ca7cf75b5
mustafaersoyekip <mustafa.ersoy@ekip.co>
2022-09-27 21:00:45 +0300

    cors

--- dist/server.js
+++ dist/server.js
@@ -14,7 +14,26 @@ const https = require("https");
 /*app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });*/
-const server = https.createServer(generateX509Certificate([
+const server = https.createServer((req, res) => {
+    const headers = {
+        "Access-Control-Allow-Origin": "*" /* @dev First, read about security */,
+        "Access-Control-Allow-Methods": "OPTIONS, POST, GET",
+        "Access-Control-Max-Age": 2592000, // 30 days
+        /** add other headers as per requirement */
+    };
+    if (req.method === "OPTIONS") {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+    }
+    if (["GET", "POST"].indexOf(req.method) > -1) {
+        res.writeHead(200, headers);
+        res.end("Hello World");
+        return;
+    }
+    res.writeHead(405, headers);
+    res.end(`${req.method} is not allowed for the request.`);
+}, generateX509Certificate([
     { type: 6, value: "http://localhost" },
     { type: 7, ip: "127.0.0.1" },
 ]), makeExpressApp());
